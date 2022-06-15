2d5df598c2fb92dea55c7bab9675b73c94a9d63c
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-06-15 00:29:43 -0300

    feat: adding component TextInput

---
+++ src/components/primitives/TextInput/index.tsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+
+interface InputDataProps {
+	title: string;
+	dataValue: string;
+	onHandleDataValue: (value: string) => void;
+	hasError: boolean;
+	isEditable?: boolean;
+}
+
+const InputData = ({
+	title,
+	dataValue,
+	onHandleDataValue,
+	hasError,
+	isEditable
+}: InputDataProps) => {
+	const [isFocused, setIsFocused] = useState(false);
+	const handleDataValue = (value: string) => {
+		onHandleDataValue(value);
+	};
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.wrapperText}>
+				<Text style={[styles.baseText, styles.title]}>{title}</Text>
+				{hasError && (
+					<Text style={[styles.baseText, styles.errorText]}>Can't be zero</Text>
+				)}
+			</View>
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={[
+						styles.input,
+						isFocused ? styles.focused : null,
+						hasError ? styles.error : null,
+					]}
+					keyboardType="ascii-capable"
+					selectionColor="hsl(183, 100%, 15%)"
+					value={dataValue}
+					onChangeText={handleDataValue}
+					onFocus={() => {
+						setIsFocused(true);
+					}}
+					onBlur={() => {
+						setIsFocused(false);
+					}}
+					editable={isEditable}
+				/>
+			</View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		alignItems: "flex-start",
+		marginVertical: 24,
+	},
+	baseText: {
+		fontWeight: "700",
+		letterSpacing: 0.6,
+		fontSize: 18,
+		marginVertical: 4,
+	},
+	wrapperText: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		width: '100%',
+	},
+	title: {
+		color: "hsl(184, 14%, 56%)",
+	},
+	inputContainer: {
+		width: "100%",
+	},
+	input: {
+		backgroundColor: "hsl(189, 41%, 97%)",
+		paddingHorizontal: 16,
+		paddingVertical: 8,
+		borderRadius: 5,
+		fontSize: 24,
+		color: "hsl(183, 100%, 15%)",
+		fontWeight: "700",
+		letterSpacing: 0.6,
+		textAlign: "right",
+	},
+	focused: {
+		borderColor: "hsl(172, 67%, 45%)",
+		borderWidth: 1,
+	},
+	errorText: {
+		color: "red",
+	},
+	error: {
+		borderColor: "red",
+		borderWidth: 1,
+	},
+});
+
+export default InputData;
---
+++ src/components/primitives/index.tsx
@@ -0,0 +1,11 @@
+import TextInput from './TextInput'
+import { MonoText } from './StyledText';
+import { Text, View } from './Themed';
+
+
+export {
+	MonoText,
+	Text,
+	TextInput,
+	View,
+}
